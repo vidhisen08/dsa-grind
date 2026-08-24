8383903e809f6a5526e41b14e44aaa4ff8c479b3
Vidhi <senvidhi761@gmail.com>
2026-08-24 16:51:16 +0530

    Time: 0 ms (100%), Space: 43 MB (15.72%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -17,28 +17,19 @@ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         List<Integer> l1 = new ArrayList<>();
         List<Integer> l2 = new ArrayList<>();
-        traverse1(p,l1);
-        traverse2(q,l2);
+        traverse(p,l1);
+        traverse(q,l2);
 
         return l1.equals(l2);
     }
 
-    public void traverse1(TreeNode node1, List<Integer> l1){
-        if(node1==null){
-            l1.add(null);
+    public void traverse(TreeNode node, List<Integer> l){
+        if(node==null){
+            l.add(null);
             return;
         }
-        l1.add(node1.val);
-        traverse1(node1.left,l1);
-        traverse1(node1.right,l1);
-    }
-    public void traverse2(TreeNode node2, List<Integer> l2){
-        if(node2==null){
-            l2.add(null);
-            return;
-        }
-        l2.add(node2.val);
-        traverse2(node2.left,l2);
-        traverse2(node2.right,l2);
+        l.add(node.val);
+        traverse(node.left,l);
+        traverse(node.right,l);
     }
 }
